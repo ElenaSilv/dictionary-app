5bcef95d0253659bdecf47164c7a6723b898c9f7
ElenaSilv <elena.silvestricecinelli@gmail.com>
2022-05-12 20:08:00 +0100

    added the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <a href="https://github.com/ElenaSilv/dictionary-app">
+            Open-source code
+          </a>{" "}
+          by Elena Silvestri Cecinelli
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
